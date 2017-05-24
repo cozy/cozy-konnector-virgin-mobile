565811f0c8fc4630d5ae17ff0484805da38495ea
Bruno Michel <bmichel@menfin.info>
2017-05-24 15:18:20 +0200

    Migrate the konnector to V3

--- konnector.js
+++ konnector.js
@@ -4,20 +4,48 @@ const request = require('request')
 const requestJSON = require('request-json')
 const moment = require('moment')
 
-const filterExisting = require('../lib/filter_existing')
-const saveDataAndFile = require('../lib/save_data_and_file')
-const localization = require('../lib/localization_manager')
-const linkBankOperation = require('../lib/link_bank_operation')
-const factory = require('../lib/base_konnector')
-
-const Bill = require('../models/bill')
-
-const logger = require('printit')({
-  prefix: 'Virginmobile',
-  date: true
-})
+const {
+    log,
+    baseKonnector,
+    filterExisting,
+    linkBankOperation,
+    saveDataAndFile,
+    models
+} = require('cozy-konnector-libs')
+const Bill = models.bill
 
 const baseURL = 'https://espaceclient.virginmobile.fr/'
+
+module.exports = baseKonnector.createNew({
+  name: 'Virgin mobile',
+  description: 'konnector description virginmobile',
+  vendorLink: 'https://www.virginmobile.fr/',
+
+  category: 'telecom',
+  color: {
+    hex: '#D72938',
+    css: '#D72938'
+  },
+
+  dataType: ['bill'],
+
+  models: [Bill],
+
+  fetchOperations: [
+    login,
+    parsePage,
+    customFilterExisting,
+    customSaveDataAndFile,
+    linkBankOperation({
+      log: log,
+      minDateDelta: 1,
+      maxDateDelta: 1,
+      model: Bill,
+      amountDelta: 0.1,
+      identifier: ['virgin mobile']
+    })
+  ]
+})
 
 const fileOptions = {
   vendor: 'Virgin mobile',
@@ -39,21 +67,21 @@ function login (requiredFields, billInfos, data, next) {
 
   const client = requestJSON.createClient(baseURL)
 
-  logger.info('Signing in')
+  log('info', 'Signing in')
   request(signInOptions, (err, res) => {
     if (err) {
-      logger.error('Signin failed')
-      return next('bad credentials')
+      log('error', 'Signin failed')
+      return next('LOGIN_FAILED')
     }
 
     client.headers.Cookie = res.headers['set-cookie']
 
     // Download bill information page.
-    logger.info('Fetching bills list')
+    log('info', 'Fetching bills list')
     client.get('api/getFacturesData', (err, res, body) => {
       if (err || !body.success) {
-        logger.error('An error occured while fetching bills list')
-        return next('no bills retrieved')
+        log('error', 'An error occured while fetching bills list')
+        return next('UNKNOWN_ERROR')
       }
 
       data.content = body.data
@@ -81,72 +109,19 @@ function parsePage (requiredFields, bills, data, next) {
     }
   }
 
-  logger.info(`${bills.fetched.length} bill(s) retrieved`)
+  log('info', `${bills.fetched.length} bill(s) retrieved`)
   next()
 }
 
 function customFilterExisting (requiredFields, entries, data, next) {
-  filterExisting(logger, Bill)(requiredFields, entries, data, next)
+  filterExisting(null, Bill)(requiredFields, entries, data, next)
 }
 
 function customSaveDataAndFile (requiredFields, entries, data, next) {
-  saveDataAndFile(logger, Bill, fileOptions, ['bill'])(
-      requiredFields, entries, data, next)
+  saveDataAndFile(null, Bill, fileOptions, ['bill'])(
+    requiredFields,
+    entries,
+    data,
+    next
+  )
 }
-
-function buildNotifContent (requiredFields, entries, data, next) {
-  if (entries.filtered && (entries.filtered.length > 0)) {
-    entries.notifContent = localization.t('notification bills', {
-      smart_count: entries.filtered.length
-    })
-  }
-
-  next()
-}
-
-module.exports = factory.createNew({
-  name: 'Virgin mobile',
-  description: 'konnector description virginmobile',
-  vendorLink: 'https://www.virginmobile.fr/',
-
-  category: 'telecom',
-  color: {
-    hex: '#D72938',
-    css: '#D72938'
-  },
-
-  fields: {
-    login: {
-      type: 'text'
-    },
-    password: {
-      type: 'password'
-    },
-    folderPath: {
-      type: 'folder',
-      advanced: true
-    }
-  },
-
-  dataType: [
-    'bill'
-  ],
-
-  models: [Bill],
-
-  fetchOperations: [
-    login,
-    parsePage,
-    customFilterExisting,
-    customSaveDataAndFile,
-    linkBankOperation({
-      log: logger,
-      minDateDelta: 1,
-      maxDateDelta: 1,
-      model: Bill,
-      amountDelta: 0.1,
-      identifier: ['virgin mobile']
-    }),
-    buildNotifContent
-  ]
-})
